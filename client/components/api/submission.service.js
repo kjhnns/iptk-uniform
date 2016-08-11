d52ade8cf2875b6d2e390b5d72367d91ecb60f1e
Johannes Klumpe <klumpe@live.de>
2016-08-11 15:37:54 +0200

    submissions service

--- client/components/api/submission.service.js
+++ client/components/api/submission.service.js
@@ -5,7 +5,31 @@
         return $resource('/api/submissions/:id/:controller', {
             id: '@_id'
         }, {
-            assigned: { method: 'GET', params: { controller: 'assigned', id: '0', isArray: true } },
+            assignedOpen: {
+                method: 'GET',
+                params: { controller: 'open', id: '0' },
+                isArray: true,
+                transformResponse: (data, _) => {
+                    var res = JSON.parse(data);
+                    var subs = [];
+                    res.Submissions.forEach((obj) => {
+                        if (obj.Reviews.length <= 0) {
+                            subs.push(obj);
+                        }
+                    });
+                    console.log(subs);
+                    return subs;
+                }
+            },
+            assigned: {
+                method: 'GET',
+                params: { controller: 'assigned', id: '0' },
+                isArray: true,
+                transformResponse: (data, _) => {
+                    var res = JSON.parse(data);
+                    return res.Submissions;
+                }
+            },
             index: { method: 'GET', isArray: true },
             create: { method: 'POST' },
             show: { method: 'GET' },
