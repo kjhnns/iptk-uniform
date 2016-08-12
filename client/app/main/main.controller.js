5f6c05b849fd2a07bc5cfa6ab446b7f2700c1a06
Johannes Klumpe <klumpe@live.de>
2016-08-12 14:50:20 +0200

    delete Thing module

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,33 +4,9 @@
 
 class MainController {
 
-  constructor($http, $scope, socket) {
-    this.$http = $http;
-    this.socket = socket;
-    this.awesomeThings = [];
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
+  constructor() {
   }
 
-  $onInit() {
-    this.$http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      this.socket.syncUpdates('thing', this.awesomeThings);
-    });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
 }
 
 angular.module('conferenceApp')
